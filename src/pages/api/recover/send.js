65476fcff897e6a21c0f47fd21d1f4803cb27f80
Guilherme Serpa Batista <serpa419@gmail.com>
2024-08-18 14:35:18 -0300

    teste email

--- src/pages/api/recover/send.js
+++ src/pages/api/recover/send.js
@@ -1,143 +1,143 @@
-import prisma from '../../../utils/prismadb'
-const nodemailer = require("nodemailer");
-import { v4 as uuidv4 } from 'uuid';
+// import prisma from '../../../utils/prismadb'
+// const nodemailer = require("nodemailer");
+// import { v4 as uuidv4 } from 'uuid';
 
-export default async function CheckEmail(req, res) {
-    const { email } = req.body
-    const transporter = nodemailer.createTransport({
-        port: 465,
-        host: "smtp.gmail.com",
-        auth: {
-            user: process.env.EMAIL,
-            pass: process.env.EMAIL_PASS,
-        },
-        secure: true,
-    });
-    try {
-        const user = await prisma.User.findUnique({
-            where: {
-                email
-            }
-        });
-        if (!user) {
-            res.status(404).send()
-        } else {
-            const createLink = await prisma.User.update({
-                where: {
-                    email
-                },
-                data: {
-                    passwordLost: uuidv4()
-                }
-            })
-            const recoverLink = `${process.env.NEXTAUTH_URL}/recover-password/${createLink.passwordLost}`
+// export default async function CheckEmail(req, res) {
+//     const { email } = req.body
+//     const transporter = nodemailer.createTransport({
+//         port: 465,
+//         host: "smtp.gmail.com",
+//         auth: {
+//             user: process.env.EMAIL,
+//             pass: process.env.EMAIL_PASS,
+//         },
+//         secure: true,
+//     });
+//     try {
+//         const user = await prisma.User.findUnique({
+//             where: {
+//                 email
+//             }
+//         });
+//         if (!user) {
+//             res.status(404).send()
+//         } else {
+//             const createLink = await prisma.User.update({
+//                 where: {
+//                     email
+//                 },
+//                 data: {
+//                     passwordLost: uuidv4()
+//                 }
+//             })
+//             const recoverLink = `${process.env.NEXTAUTH_URL}/recover-password/${createLink.passwordLost}`
 
-            const html = `
-            <!DOCTYPE html>
-<html lang="pt-br">
+//             const html = `
+//             <!DOCTYPE html>
+// <html lang="pt-br">
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Recuperação de Senha</title>
-  <style>
-    body {
-      font-family: Arial, sans-serif;
-      background-color: #f7f7f7;
-      padding: 20px;
-    }
+// <head>
+//   <meta charset="UTF-8">
+//   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//   <title>Recuperação de Senha</title>
+//   <style>
+//     body {
+//       font-family: Arial, sans-serif;
+//       background-color: #f7f7f7;
+//       padding: 20px;
+//     }
     
-    .container {
-      max-width: 600px;
-      margin: 0 auto;
-      background-color: #ffffff;
-      padding: 40px;
-      border-radius: 4px;
-      box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-    }
+//     .container {
+//       max-width: 600px;
+//       margin: 0 auto;
+//       background-color: #ffffff;
+//       padding: 40px;
+//       border-radius: 4px;
+//       box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+//     }
     
-    h1 {
-      text-align: center;
-      color: #333333;
-    }
+//     h1 {
+//       text-align: center;
+//       color: #333333;
+//     }
     
-    p {
-      margin-bottom: 20px;
-      color: #666666;
-    }
+//     p {
+//       margin-bottom: 20px;
+//       color: #666666;
+//     }
     
-    .btn {
-      display: inline-block;
-      background-color: #4CAF50;
-      color: #ffffff;
-      text-decoration: none;
-      padding: 10px 20px;
-      border-radius: 4px;
-    }
+//     .btn {
+//       display: inline-block;
+//       background-color: #4CAF50;
+//       color: #ffffff;
+//       text-decoration: none;
+//       padding: 10px 20px;
+//       border-radius: 4px;
+//     }
     
-    .btn:hover {
-      background-color: #45a049;
-    }
-  </style>
-</head>
+//     .btn:hover {
+//       background-color: #45a049;
+//     }
+//   </style>
+// </head>
 
-<body>
-  <div class="container">
-    <h1>Recuperação de Senha - FENEG </h1>
-    <p>Olá,${createLink.name}</p>
-    <p>Recebemos uma solicitação de recuperação de senha para a sua conta. Se você não solicitou essa recuperação, por favor, ignore este e-mail.</p>
-    <p>Caso tenha sido você, clique no botão abaixo para redefinir a sua senha:</p>
-    <p>
-      <a href="${recoverLink}" class="btn">Redefinir Senha</a>
-    </p>
-    <p>Ou copie e cole o seguinte link no seu navegador:</p>
-    <p><a href="${recoverLink}">${recoverLink}</a></p>
-    <p>Obrigado,</p>
-    <p>A equipe de suporte</p>
-  </div>
-</body>
+// <body>
+//   <div class="container">
+//     <h1>Recuperação de Senha - FENEG </h1>
+//     <p>Olá,${createLink.name}</p>
+//     <p>Recebemos uma solicitação de recuperação de senha para a sua conta. Se você não solicitou essa recuperação, por favor, ignore este e-mail.</p>
+//     <p>Caso tenha sido você, clique no botão abaixo para redefinir a sua senha:</p>
+//     <p>
+//       <a href="${recoverLink}" class="btn">Redefinir Senha</a>
+//     </p>
+//     <p>Ou copie e cole o seguinte link no seu navegador:</p>
+//     <p><a href="${recoverLink}">${recoverLink}</a></p>
+//     <p>Obrigado,</p>
+//     <p>A equipe de suporte</p>
+//   </div>
+// </body>
 
-</html>`
+// </html>`
 
-            const mailData = {
-                from: {
-                    name: `FENEG  - Sicoob Frutal`,
-                    address: "feneg@gmail.com",
-                },
-                to: email,
-                subject: `Recuperar Senha - FENEG `,
-                html: html,
-            };
+//             const mailData = {
+//                 from: {
+//                     name: `FENEG  - Sicoob Frutal`,
+//                     address: "feneg@gmail.com",
+//                 },
+//                 to: email,
+//                 subject: `Recuperar Senha - FENEG `,
+//                 html: html,
+//             };
 
-            await new Promise((resolve, reject) => {
-                // verify connection configuration
-                transporter.verify(function (error, success) {
-                    if (error) {
-                        console.log(error);
-                        reject(error);
-                    } else {
-                        console.log("Server is ready to take our messages");
-                        resolve(success);
-                    }
-                });
-            });
+//             await new Promise((resolve, reject) => {
+//                 // verify connection configuration
+//                 transporter.verify(function (error, success) {
+//                     if (error) {
+//                         console.log(error);
+//                         reject(error);
+//                     } else {
+//                         console.log("Server is ready to take our messages");
+//                         resolve(success);
+//                     }
+//                 });
+//             });
 
-            await new Promise((resolve, reject) => {
-                // send mail
-                transporter.sendMail(mailData, (err, info) => {
-                    if (err) {
-                        console.error(err);
-                        reject(err);
-                    } else {
-                        console.log(info);
-                        resolve(info);
-                    }
-                });
-            });
+//             await new Promise((resolve, reject) => {
+//                 // send mail
+//                 transporter.sendMail(mailData, (err, info) => {
+//                     if (err) {
+//                         console.error(err);
+//                         reject(err);
+//                     } else {
+//                         console.log(info);
+//                         resolve(info);
+//                     }
+//                 });
+//             });
 
-            res.status(200).json({ status: "OK" });
-        }
-    } catch (error) {
-        res.status(400).json({ message: error })
-    }
-}
+//             res.status(200).json({ status: "OK" });
+//         }
+//     } catch (error) {
+//         res.status(400).json({ message: error })
+//     }
+// }
--- src/pages/forgot-password.js
+++ src/pages/forgot-password.js
@@ -1,145 +1,145 @@
-import { forwardRef, useState } from 'react';
-import { useForm } from 'react-hook-form';
-import { signIn, useSession } from 'next-auth/react';
-import { useRouter } from 'next/router';
-import {
-  Box,
-  Container,
-  CssBaseline,
-  Grid,
-  Link,
-  TextField,
-  Typography,
-  createTheme,
-  ThemeProvider,
-  Alert,
-} from '@mui/material';
-import LoadingButton from '@mui/lab/LoadingButton';
-import { styled } from '@mui/system';
-import Copyright from '../components/copyright'
-import Image from 'next/image';
-import Head from 'next/head';
-import axios from 'axios';
-import Snackbar from '@mui/material/Snackbar';
-import MuiAlert from '@mui/material/Alert';
-import { useEffect } from 'react';
+// import { forwardRef, useState } from 'react';
+// import { useForm } from 'react-hook-form';
+// import { signIn, useSession } from 'next-auth/react';
+// import { useRouter } from 'next/router';
+// import {
+//   Box,
+//   Container,
+//   CssBaseline,
+//   Grid,
+//   Link,
+//   TextField,
+//   Typography,
+//   createTheme,
+//   ThemeProvider,
+//   Alert,
+// } from '@mui/material';
+// import LoadingButton from '@mui/lab/LoadingButton';
+// import { styled } from '@mui/system';
+// import Copyright from '../components/copyright'
+// import Image from 'next/image';
+// import Head from 'next/head';
+// import axios from 'axios';
+// import Snackbar from '@mui/material/Snackbar';
+// import MuiAlert from '@mui/material/Alert';
+// import { useEffect } from 'react';
 
-const defaultTheme = createTheme();
+// const defaultTheme = createTheme();
 
-const ErrorText = styled(Typography)`
-  color: red;
-`;
+// const ErrorText = styled(Typography)`
+//   color: red;
+// `;
 
-export default function ForgotPassword() {
-  const router = useRouter();
-  const { handleSubmit, register, formState: { errors } } = useForm();
-  const [loading, setLoading] = useState(false);
-  const [open, setOpen] = useState(false);
-  const [errorRecover, setErrorRecover] = useState('');
+// export default function ForgotPassword() {
+//   const router = useRouter();
+//   const { handleSubmit, register, formState: { errors } } = useForm();
+//   const [loading, setLoading] = useState(false);
+//   const [open, setOpen] = useState(false);
+//   const [errorRecover, setErrorRecover] = useState('');
 
 
-  const handleClose = (event, reason) => {
-    if (reason === 'clickaway') {
-      return;
-    }
+//   const handleClose = (event, reason) => {
+//     if (reason === 'clickaway') {
+//       return;
+//     }
 
-    setOpen(false);
-  };
+//     setOpen(false);
+//   };
 
-  const onSubmit = async (data) => {
-    setErrorRecover('')
-    setLoading(true)
-    try {
-      const sendEmail = await axios.post('/api/recover/send', { email: data.email })
-      setLoading(false)
-      if (sendEmail.status === 200) {
-        setOpen(true);
-        router.push('/login')
-      }
-    } catch (error) {
-      if (error.response.status === 404) {
-        setErrorRecover('E-mail não encontrado!')
-      } else {
-        setErrorRecover('Erro ao enviar e-mail, procure suporte!')
-      }
-      setLoading(false)
-    }
-  };
+//   const onSubmit = async (data) => {
+//     setErrorRecover('')
+//     setLoading(true)
+//     try {
+//       const sendEmail = await axios.post('/api/recover/send', { email: data.email })
+//       setLoading(false)
+//       if (sendEmail.status === 200) {
+//         setOpen(true);
+//         router.push('/login')
+//       }
+//     } catch (error) {
+//       if (error.response.status === 404) {
+//         setErrorRecover('E-mail não encontrado!')
+//       } else {
+//         setErrorRecover('Erro ao enviar e-mail, procure suporte!')
+//       }
+//       setLoading(false)
+//     }
+//   };
 
-  const Alert = forwardRef(function Alert(props, ref) {
-    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
-  });
+//   const Alert = forwardRef(function Alert(props, ref) {
+//     return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+//   });
 
-  return (
-    <ThemeProvider theme={defaultTheme}>
-      <Head>
-        <title>Esqueci a Senha - FENEG - Sicoob Frutal</title>
-      </Head>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Image alt='Sicoob Logo' width={50} height={50} src={require('../../public/sicoob.png')} />
-          <Typography component="h1" variant="h5">
-            Recuperar Senha
-          </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit(onSubmit)} sx={{ mt: 3, width: '100%' }}>
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="E-mail"
-                  name="email"
-                  {...register('email', {
-                    required: 'Este campo é obrigatório',
-                    pattern: {
-                      value: /\S+@\S+\.\S+/,
-                      message: 'Endereço de e-mail inválido',
-                    },
-                  })}
-                  autoComplete="email"
-                />
-                {errors.email && <ErrorText>Este campo é obrigatório</ErrorText>}
-              </Grid>
-            </Grid>
-            {errorRecover ? (<Alert severity="error">{errorRecover}</Alert>) : false}
-            <LoadingButton
-              loading={loading}
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{
-                mt: 3, mb: 2, backgroundColor: '#49479D', "&:hover": {
-                  backgroundColor: '#003641',
-                },
-              }}
-            >
-              Enviar
-            </LoadingButton>
-            <Snackbar open={open} autoHideDuration={5000} onClose={handleClose}>
-              <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
-                E-mail enviado com sucesso, verifique sua caixa de e-mail para recuperar o acesso a sua conta!
-              </Alert>
-            </Snackbar>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Link href="/register" variant="body2">
-                  Ainda não é cadastrado? Cadastre-se
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-        <Copyright sx={{ mt: 5 }} />
-      </Container>
-    </ThemeProvider>
-  );
-}
+//   return (
+//     <ThemeProvider theme={defaultTheme}>
+//       <Head>
+//         <title>Esqueci a Senha - FENEG - Sicoob Frutal</title>
+//       </Head>
+//       <Container component="main" maxWidth="xs">
+//         <CssBaseline />
+//         <Box
+//           sx={{
+//             marginTop: 8,
+//             display: 'flex',
+//             flexDirection: 'column',
+//             alignItems: 'center',
+//           }}
+//         >
+//           <Image alt='Sicoob Logo' width={50} height={50} src={require('../../public/sicoob.png')} />
+//           <Typography component="h1" variant="h5">
+//             Recuperar Senha
+//           </Typography>
+//           <Box component="form" noValidate onSubmit={handleSubmit(onSubmit)} sx={{ mt: 3, width: '100%' }}>
+//             <Grid container spacing={2}>
+//               <Grid item xs={12}>
+//                 <TextField
+//                   required
+//                   fullWidth
+//                   id="email"
+//                   label="E-mail"
+//                   name="email"
+//                   {...register('email', {
+//                     required: 'Este campo é obrigatório',
+//                     pattern: {
+//                       value: /\S+@\S+\.\S+/,
+//                       message: 'Endereço de e-mail inválido',
+//                     },
+//                   })}
+//                   autoComplete="email"
+//                 />
+//                 {errors.email && <ErrorText>Este campo é obrigatório</ErrorText>}
+//               </Grid>
+//             </Grid>
+//             {errorRecover ? (<Alert severity="error">{errorRecover}</Alert>) : false}
+//             <LoadingButton
+//               loading={loading}
+//               type="submit"
+//               fullWidth
+//               variant="contained"
+//               sx={{
+//                 mt: 3, mb: 2, backgroundColor: '#49479D', "&:hover": {
+//                   backgroundColor: '#003641',
+//                 },
+//               }}
+//             >
+//               Enviar
+//             </LoadingButton>
+//             <Snackbar open={open} autoHideDuration={5000} onClose={handleClose}>
+//               <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+//                 E-mail enviado com sucesso, verifique sua caixa de e-mail para recuperar o acesso a sua conta!
+//               </Alert>
+//             </Snackbar>
+//             <Grid container justifyContent="flex-end">
+//               <Grid item>
+//                 <Link href="/register" variant="body2">
+//                   Ainda não é cadastrado? Cadastre-se
+//                 </Link>
+//               </Grid>
+//             </Grid>
+//           </Box>
+//         </Box>
+//         <Copyright sx={{ mt: 5 }} />
+//       </Container>
+//     </ThemeProvider>
+//   );
+// }
